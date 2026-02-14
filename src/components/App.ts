46d0b64f77a4249c6b62a2e6a897c9177afaac7c
Jinseong Jeon <aimer120@nate.com>
2026-02-15 01:56:14 +0900

    feat: add App component

--- src/components/App.ts
+++ src/components/App.ts
@@ -1,58 +1,66 @@
 import type { Modal } from '../modal';
 import type { List } from './List';
-import ComponentBase, { type Component } from '../component-base.js';
-import ButtonComponent, { type Button } from './Button.js';
+import ComponentBase, { type Component } from './component-base.js';
 import ListComponent from './List.js';
+import ButtonComponent, { type Button } from './Button.js';
+import AddFormComponent, { type AddForm } from './AddForm.js';
+import ControlComponent, { ControlType } from './Control.js';
+import ItemComponent from './item/Item.js';
+import { ItemType } from './item/item.interface.js';
 import { CLASS_NAME } from '../constants.js';
 
-const MenuType = {
-  image: 'image',
-  video: 'video',
-  note: 'note',
-  task: 'task',
-} as const;
-
-type MenuType = (typeof MenuType)[keyof typeof MenuType];
-type Menu = { label: string; type: MenuType };
+type Menu = { label: string; type: ItemType };
 
 const MENUS: Menu[] = [
-  { label: 'IMAGE', type: 'image' },
-  { label: 'VIDEO', type: 'video' },
-  { label: 'NOTE', type: 'note' },
-  { label: 'TASK', type: 'task' },
+  { label: 'IMAGE', type: ItemType.image },
+  { label: 'VIDEO', type: ItemType.video },
+  { label: 'NOTE', type: ItemType.text },
+  { label: 'TASK', type: ItemType.text },
 ];
 
-type AppProps = Record<string, unknown>;
+const FormLabel = {
+  title: 'Title',
+  body: 'Body',
+} as const;
+
+type FormLabel = (typeof FormLabel)[keyof typeof FormLabel];
+type FormData = Record<FormLabel, string>;
+type AppProps = Readonly<Record<string, unknown>>;
 
 export default class AppComponent extends ComponentBase<
   HTMLElement,
   AppProps,
   Component<any, any>
 > {
-  private list: List | null = null;
+  private list!: List;
 
   constructor(
     initialProps: AppProps = {} as AppProps,
     private modal: Modal,
   ) {
     super(initialProps);
-  }
-
-  protected override beforeHostElementBuild(): void {
-    this.list = new ListComponent();
+    this.appendChildren(this.host!);
   }
 
   protected createHostElement(): HTMLElement {
     const hostEl = document.createElement<'section'>('section');
     hostEl.className = CLASS_NAME.app;
-
-    hostEl.prepend(this.createHeader());
-    this.list!.render(hostEl);
-
     return hostEl;
   }
 
-  private createHeader(): HTMLElement {
+  private appendChildren(host: HTMLElement): void {
+    const menuButtons = MENUS.map<Button>(({ label, type }) =>
+      new ButtonComponent({
+        label,
+        classList: [CLASS_NAME.menuButton],
+      }).setClickHandler(() => this.modal.show(this.createAddFormByType(type))),
+    );
+
+    host.prepend(this.createHeader(menuButtons));
+    this.list = new ListComponent({}).render(host);
+  }
+
+  private createHeader(menuButtons: Button[]): HTMLElement {
     const headerEl = document.createElement<'header'>('header');
     headerEl.className = CLASS_NAME.appHeader;
 
@@ -60,16 +68,57 @@ export default class AppComponent extends ComponentBase<
     h1.className = CLASS_NAME.appTitle;
     h1.textContent = 'MOTION';
 
-    const menuButtons = MENUS.map<Button>(
-      ({ label }) =>
-        new ButtonComponent({
-          label,
-          classList: [CLASS_NAME.menuButton],
-        }).setClickHandler(() => this.modal.show(null)), // TODO: input form & control
-    );
-
     headerEl.prepend(h1);
     menuButtons.forEach((btn) => btn.render(headerEl));
     return headerEl;
   }
+
+  private createAddFormByType(type: ItemType): AddForm {
+    return new AddFormComponent({})
+      .setChildren([
+        new ControlComponent({ label: 'Title' }),
+        new ControlComponent({
+          label: 'Body',
+          type: itemTypeToControlType(type),
+        }),
+      ])
+      .setSubmitHandler((data) => {
+        this.list.add(
+          this.createItem(type, data as FormData) //
+            .setRemoveHandler((item) => this.list.remove(item)),
+        );
+        this.modal.hide();
+      });
+  }
+
+  private createItem(type: ItemType, data: FormData): ItemComponent {
+    const { Title: title, Body: body } = data;
+    switch (type) {
+      case ItemType.image:
+        return new ItemComponent({ type, title, url: body, alt: title });
+      case ItemType.video:
+        return new ItemComponent({ type, title, url: body });
+      case ItemType.text:
+        return new ItemComponent({ type, title, description: body });
+      default:
+        const neverType: never = type;
+        throw new Error(`Not supported type ${neverType}`);
+    }
+  }
 }
+
+function itemTypeToControlType(itemType: ItemType): ControlType {
+  switch (itemType) {
+    case ItemType.image:
+    case ItemType.video:
+      return ControlType.url;
+    case ItemType.text:
+      return ControlType.textarea;
+    default:
+      const neverType: never = itemType;
+      throw new Error(`Not supported type ${neverType}`);
+  }
+}
+
+// https://fastly.picsum.photos/id/786/700/350.jpg?hmac=eI1TG9CaWM15B1mycXUjh88-YmKVkdx5GHLxXG43sJ4
+// https://www.youtube.com/embed/hYlBSiX28ps
